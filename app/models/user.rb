c4a76214b37003aa877c12ec5cf36f27d9fcef13
paulzay <paulotieno2@gmail.com>
2020-08-13 17:54:26 +0300

    add username field for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
-  # validates :name, presence: true
-  # validates :name, length: { minimum: 2 }
+  validates :name, presence: true, length: { minimum: 4, maximum: 16 }
   validates :email, confirmation: true 
   validates :email, presence: true
   validates :password, presence: true
